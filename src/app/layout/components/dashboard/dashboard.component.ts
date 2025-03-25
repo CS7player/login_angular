ffd8b04867388694b4c4d6c6870599eef36ba093
chandra <chandrasekhar@bijib.in>
2025-03-25 17:09:45 +0530

    outside click

--- src/app/layout/components/dashboard/dashboard.component.ts
+++ src/app/layout/components/dashboard/dashboard.component.ts
@@ -1,4 +1,5 @@
-import { Component } from '@angular/core';
+import { Component, inject, OnInit } from '@angular/core';
+import { CloseClickService } from '../../../utils/close-click.service';
 
 @Component({
  selector: 'app-dashboard',
@@ -6,6 +7,16 @@ import { Component } from '@angular/core';
  templateUrl: './dashboard.component.html',
  styleUrls: []
 })
-export class DashboardComponent {
+export class DashboardComponent implements OnInit{
+ private closeClick = inject(CloseClickService);
+ ngOnInit(){
+  this.outSideClick();
+ }
+
+ outSideClick() {
+  this.closeClick.closeclickEmitter.subscribe(() => {
+   console.log('testing');
+  });
+}
 
 }
---
+++ src/app/utils/close-click.service.ts
@@ -0,0 +1,26 @@
+import { EventEmitter, Injectable } from '@angular/core';
+
+@Injectable({
+ providedIn: 'root'
+})
+export class CloseClickService {
+ closeclickEmitter: EventEmitter<any> = new EventEmitter<any>();
+ constructor() {
+  this.listenToDocClick();
+ }
+ private listenToDocClick() {
+  document.addEventListener("click", (event) => {
+   const target = event.target as HTMLElement;
+   const closeElements = document.querySelectorAll('#close-code');
+   const clickedInsideCustomClose = Array.from(closeElements).some((element) => element.contains(target));
+   if (!clickedInsideCustomClose) {
+    console.log(closeElements);
+    this.onClicking();
+   }
+  })
+ }
+
+ onClicking(){
+  this.closeclickEmitter.emit();
+ }
+}
